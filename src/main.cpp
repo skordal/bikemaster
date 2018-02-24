15c78dd9fd19b2fa16d47a187f297c9b14b4fff6
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2018-02-25 00:31:48 +0100

    Add processor and SDRAM initialization code to main()

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,31 @@
 #include "clock.h"
 #include "debug.h"
 #include "gpio.h"
+#include "sdram.h"
 
 int main()
 {
 	Debug::stream() << "Bikemaster initializing..." << Debug::endl();
+
+	// Configure some processor settings:
+	SCB->SCR |= SCB_SCR_SEVONPEND_Msk; // Events are sent on pending interrupts
+	SCB_EnableICache();                // Enable instruction cache
+	SCB_EnableDCache();                // Enable data cache
+
+	// Enable access to FPU registers:
+	SCB->CPACR |= 0xfu << 20;
+	// Enable automatic (lazy) saving of floating point state in interrupts:
+	FPU->FPCCR |= FPU_FPCCR_ASPEN_Msk | FPU_FPCCR_LSPEN_Msk;
+	__DSB(); __ISB();
+
+	// Enable interrupts, some initialization procedures use interrupts early on:
+	__enable_irq();
+
+	// Set up all the clocks:
 	Clock::initialize();
+
+	// Enable the external SDRAM:
+	SDRAM::initialize();
 
 	// Turn on the status LED:
 	GPIO::Pin statusLED(1, GPIO::Port::I);
