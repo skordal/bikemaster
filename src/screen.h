ef94108dee9932fcdc67a0c1fb199753dba32791
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2018-03-02 22:22:10 +0100

    Add base class for GUI screens

---
+++ src/screen.h
@@ -0,0 +1,17 @@
+// Bikemaster - A bike statistics solution
+// (c) Kristian Klomsten Skordal 2018 <kristian.skordal@wafflemail.net>
+// Report bugs and issues on <https://github.com/skordal/bikemaster/issues>
+
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "framebuffer.h"
+
+class Screen
+{
+	public:
+		virtual void render(Framebuffer & fb) = 0;
+		virtual Color getBackground() const { return Color::black(); }
+};
+
+#endif
