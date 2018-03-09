7d39bcc15e29c4b0126a3a68fc1a28d7d79733f9
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2018-03-09 14:41:58 +0100

    Add utility module with drawing functions

---
+++ src/utils.cpp
@@ -0,0 +1,146 @@
+// Bikemaster - A bike statistics solution
+// (c) Kristian Klomsten Skordal 2018 <kristian.skordal@wafflemail.net>
+// Report bugs and issues on <https://github.com/skordal/bikemaster/issues>
+
+#include "utils.h"
+
+// Private namespace members:
+namespace Utils
+{
+	static void plotLineNegative(Framebuffer & fb, const Point & start, const Point & end, const Color & color);
+	static void plotLinePositive(Framebuffer & fb, const Point & start, const Point & end, const Color & color);
+}
+
+void Utils::plotLineNegative(Framebuffer & fb, const Point & start, const Point & end, const Color & color)
+{
+	int dx = end.getX() - start.getX();
+	int dy = end.getY() - start.getY();
+	int yi = dy < 0 ? -1 : 1;
+
+	if(dy < 0)
+		dy = -dy;
+
+	int D = 2 * dy - dx;
+	int y = start.getY();
+
+	for(unsigned int x = start.getX(); x < end.getX(); ++x)
+	{
+		fb.setPixel(Point(x, y), color);
+		if(D > 0)
+		{
+			y += yi;
+			D -= 2 * dx;
+		}
+
+		D += 2 * dy;
+	}
+}
+
+void Utils::plotLinePositive(Framebuffer & fb, const Point & start, const Point & end, const Color & color)
+{
+	int dx = end.getX() - start.getX();
+	int dy = end.getY() - start.getY();
+	int xi = dx < 0 ? -1 : 1;
+
+	if(dx < 0)
+		dx = -dx;
+
+	int D = 2 * dx - dy;
+	int x = start.getX();
+
+	for(unsigned int y = start.getY(); y < end.getY(); ++y)
+	{
+		fb.setPixel(Point(x, y), color);
+		if(D > 0)
+		{
+			x += xi;
+			D -= 2 * dy;
+		}
+
+		D += 2 * dx;
+	}
+}
+
+void Utils::drawLine(Framebuffer & fb, const Point & start, const Point & end, const Color & color)
+{
+	// Convert coordinates to signed integers:
+	int x0 = start.getX(), y0 = start.getY();
+	int x1 = end.getX(), y1 = end.getY();
+
+	if(abs(y1 - y0) < abs(x1 - x0))
+	{
+		if(x0 > x1)
+			Utils::plotLineNegative(fb, end, start, color);
+		else
+			Utils::plotLineNegative(fb, start, end, color);
+	} else {
+		if(y0 > y1)
+			Utils::plotLinePositive(fb, end, start, color);
+		else
+			Utils::plotLinePositive(fb, start, end, color);
+	}
+}
+
+
+void Utils::drawCircle(Framebuffer & fb, const Point & origin, unsigned int r, const Color & color,
+	Utils::CircleDrawingMode mode)
+{
+	bool drawUpper = mode == Utils::CircleDrawingMode::FULL
+		|| mode == Utils::CircleDrawingMode::UPPER_SEMICIRCLE;
+	bool drawLower = mode == Utils::CircleDrawingMode::FULL
+		|| mode == Utils::CircleDrawingMode::LOWER_SEMICIRCLE;
+
+	int plotX = r, plotY = 0;
+	int p = 1 - r;
+
+	fb.setPixel(Point(origin.getX() - r, origin.getY()), color);
+	fb.setPixel(Point(origin.getX() + r, origin.getY()), color);
+
+	if(drawLower)
+		fb.setPixel(Point(origin.getX(), origin.getY() + r), color);
+	if(drawUpper)
+		fb.setPixel(Point(origin.getX(), origin.getY() - r), color);
+
+	while(plotX > plotY)
+	{
+		++plotY;
+
+		if(p <= 0)
+			p = p + 2 * plotY + 1;
+		else {
+			--plotX;
+			p = p + 2 * plotY - 2 * plotX + 1;
+		}
+
+		if(plotX < plotY)
+			break;
+
+		if(drawLower)
+		{
+			fb.setPixel(Point(plotX + origin.getX(), plotY + origin.getY()), color);
+			fb.setPixel(Point(-plotX + origin.getX(), plotY + origin.getY()), color);
+		}
+
+		if(drawUpper)
+		{
+			fb.setPixel(Point(plotX + origin.getX(), -plotY + origin.getY()), color);
+			fb.setPixel(Point(-plotX + origin.getX(), -plotY + origin.getY()), color);
+		}
+
+		if(plotX != plotY)
+		{
+			if(drawLower)
+			{
+				fb.setPixel(Point(plotY + origin.getX(), plotX + origin.getY()), color);
+				fb.setPixel(Point(-plotY + origin.getX(), plotX + origin.getY()), color);
+			}
+
+			if(drawUpper)
+			{
+				fb.setPixel(Point(plotY + origin.getX(), -plotX + origin.getY()), color);
+				fb.setPixel(Point(-plotY + origin.getX(), -plotX + origin.getY()), color);
+			}
+		}
+	}
+}
+
---
+++ src/utils.h
@@ -0,0 +1,35 @@
+// Bikemaster - A bike statistics solution
+// (c) Kristian Klomsten Skordal 2018 <kristian.skordal@wafflemail.net>
+// Report bugs and issues on <https://github.com/skordal/bikemaster/issues>
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "color.h"
+#include "framebuffer.h"
+#include "point.h"
+
+namespace Utils
+{
+	enum class CircleDrawingMode
+	{
+		UPPER_SEMICIRCLE,
+		LOWER_SEMICIRCLE,
+		FULL
+	};
+
+    void drawCircle(Framebuffer & fb, const Point & origin, unsigned int r, const Color & color,
+		CircleDrawingMode mode = CircleDrawingMode::FULL);
+	void drawLine(Framebuffer & fb, const Point & start, const Point & end,
+			const Color & color);
+
+	template<typename T> T abs(T value)
+	{
+		if(value < 0)
+			return -value;
+		else
+			return value;
+	}
+}
+
+#endif
