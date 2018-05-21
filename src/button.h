4e06563c7167854e22dd47c8477f5b8d11c458b4
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2018-05-21 14:42:11 +0200

    Allow the origin of a button to be updated after construction

--- src/button.h
+++ src/button.h
@@ -15,10 +15,17 @@ class Button final : public TouchscreenListener
 	public:
 		static constexpr const float RADIUS = 30;
 
+		Button(const Image & image, void (*callback)(void *), void * data = nullptr)
+			: Button(Point(0, 0), image, callback, data) {}
 		Button(const Point & origin, const Image & image, void (*callback)(void *), void * data = nullptr);
+
+		void setOrigin(const Point & origin) { this->origin = origin; }
+		const Point & getOrigin() const { return origin; }
 
 		void render(Framebuffer & fb);
 		void handleTouchscreenEvent(const TouchscreenEvent & event) override;
+
+		void trigger() const { if(callback != nullptr) callback(data); }
 	private:
 		Point origin;
 		const Image & image;
