601be70840e15de33e6fa9063c5b8968eaaa37f9
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2018-08-06 22:56:45 +0200

    Add #ifdefs to make it possible to disable debug logging

--- src/debug.cpp
+++ src/debug.cpp
@@ -10,18 +10,24 @@ Debug Debug::debugStream;
 
 Debug & operator<<(Debug & stream, const char * string)
 {
+#ifdef ENABLE_DEBUG
 	debug_printf(string);
+#endif
 	return stream;
 }
 
 Debug & operator<<(Debug & stream, unsigned int i)
 {
+#ifdef ENABLE_DEBUG
 	debug_printf("0x%.08x", i);
+#endif
 	return stream;
 }
 
 Debug & operator<<(Debug & stream, float f)
 {
+#ifdef ENABLE_DEBUG
 	debug_printf("%f", f);
+#endif
 	return stream;
 }
